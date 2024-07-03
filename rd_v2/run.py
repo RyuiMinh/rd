f651a9b9b73e307f80f8e8c09839cd50a19b0c43
RyuiMinh <167089278+RyuiMinh@users.noreply.github.com>
2024-07-03 16:08:30 +0700

    Update run.py

--- rd_v2/run.py
+++ rd_v2/run.py
@@ -1,14 +1,30 @@
 import os
 import subprocess
-import shutil
 
-CRD_SSH_Code = input("Google CRD SSH Code :")
+CRD_SSH_Code = input("Google CRD SSH Code: ")
 username = "ns"
 password = "root"
-os.system(f"useradd -m {username}")
-os.system(f"adduser {username} sudo")
-os.system(f"echo '{username}:{password}' | sudo chpasswd")
-os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd")
+Pin = 654321
+
+class CRDSetup:
+    def __init__(self, user):
+        os.system("apt update")
+        self.finish(user)
+
+    @staticmethod
+    def finish(user):
+        os.system(f"adduser {user} chrome-remote-desktop")
+        command = f"{CRD_SSH_Code} --pin={Pin}"
+        os.system(f"su - {user} -c '{command}'")
+        os.system("service chrome-remote-desktop start")
+        print(" ..........................................................")
+        print(f"Log in PIN : {Pin}")
+        print(f"User Name : {user}")
+        print(f"User Pass : {password}")
+        while True:
+            pass
+
+
 try:
     if CRD_SSH_Code == "":
         print("Please enter authcode from the given link")
@@ -18,23 +34,3 @@ try:
         CRDSetup(username)
 except NameError as e:
     print("'username' variable not found, Create a user first")
-
-Pin = 654321
-
-class CRDSetup:
-    def __init__(self, user):
-        os.system("apt update")
-        self.finish(user)
-        
-    @staticmethod
-    def finish(user):
-        os.system(f"adduser {user} chrome-remote-desktop")
-        command = f"{CRD_SSH_Code} --pin={Pin}"
-        os.system(f"su - {user} -c '{command}'")
-        os.system("service chrome-remote-desktop start")
-        print(" ..........................................................")
-        print("Log in PIN : 654321") 
-        print("User Name : user") 
-        print("User Pass : root") 
-        while True:
-            pass
